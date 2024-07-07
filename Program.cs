94501870ab2cd5a2c1d0729f21a0fba8efaa13f5
Victoria Sergeeva <viktime.006@gmail.com>
2024-07-07 16:12:43 +0300

    Заполнили тестовыми данными массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+string[] array = new string[] {"Hello", "world", "2", "32", "the", "dog", "Russia"};
